f1f30fe6565096feecfa0d7b8cb3ba289437081c
Shota Aizawa <someone@nowhere.com>
2015-11-26 17:58:28 -0500

    Included arduino sensor
    By using jenny-five, developed a code for Arduino

--- postcard/app.js
+++ postcard/app.js
@@ -75,3 +75,101 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 
 var io = require("socket.io").listen(server);
 
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+var port = new SerialPort("/dev/ttyAMA0", {
+  baudrate: 9600,
+  parser: serialport.parsers.readline("\n")
+}, false); // this is the openImmediately flag [default is true]
+
+
+
+
+    // Arduino Setting 
+
+var five = require("johnny-five");
+var board = new five.Board();
+var sensor;
+var socket;
+
+board.on("ready", function() {
+    // set up an analog pin and duration
+    sensor = new five.Sensor({
+        pin: "A0",
+        freq: 700
+    });
+
+    
+    // add a sensor with permission
+    board.repl.inject({
+        pot: sensor
+    });
+
+    // transmit input to numbers from 0 to 100
+    sensor.scale(0, 100).on("data", function() {
+        
+        console.log(this.value);
+
+    
+
+        // Socket.IOで値を"sensor"というイベント名で送信
+    //      socket.emit("sensor", { value: value });
+    });
+
+    
+    sensor.within([80, 100], function() {
+        console.log("package is delivered!");
+    });
+
+});
+
+
+io.sockets.on('connection', function (socket) {
+    
+      //open port for sensor data
+      port.open(function(error) {
+
+              if (error) {
+                console.log('failed to open: ' + error);
+            } else {
+                  console.log('Serial open');
+
+                  socket.on('open', function () {
+                    // console.log(data.open);
+                    // port.write('A');
+                    port.write('O');
+                    // port.write('A');
+
+                  });
+
+                  port.on('data', function(data) {
+                  console.log(data);
+
+                  });
+            }
+
+      });
+
+
+
+
+      // Send data to Parse
+
+      // socket.on('sendToParse', function (data) {
+      //       console.log(data.name);
+      //       console.log(data.password);
+
+      //       appParse.insert('post', { name: data.name, password: data.password }, function (err, response) {
+      //       console.log("entry made");
+
+      //       appParse.find('post', '' , function (err, response) {
+      //       // console.log(response);
+      //       socket.emit('toScreen',{ name: data.name });
+
+      //             });
+      //         });
+      //   });//socket on 'parse' end Bracket
+
+});
+
+
--- postcard/public/sketch.js
+++ postcard/public/sketch.js
@@ -1,18 +1,36 @@
-// Learning Processing
-// Daniel Shiffman
-// http://www.learningprocessing.com
-
-// Example 1-1: stroke and fill
+var a = 80;  // Create a global variable "a"
 
 function setup() {
   createCanvas(720, 400);
+  background(0);
   stroke(255);
-  noFill();
+  noLoop();
 }
 
 function draw() {
-  background(0);
-  for (var i = 0; i < 200; i += 20) {
-    bezier(mouseX-(i/2.0), 40+i, 410, 20, 440, 300, 240-(i/16.0), 300+(i/8.0));
+  // Draw a line using the global variable "a"
+  line(a, 0, a, height);
+  
+  // Create a new variable "a" local to the for() statement 
+  for (var a = 120; a < 200; a += 3) {
+    line(a, 0, a, height);
   }
+  
+  // Create a new variable "a" local to the draw() function
+  var a = 300;
+  // Draw a line using the new local variable "a"
+  line(a, 0, a, height);  
+  
+  // Make a call to the custom function drawAnotherLine()
+  drawAnotherLine();
+  
+  
 }
+
+function drawAnotherLine() {
+  // Create a new variable "a" local to this method
+  var a = 320;
+  // Draw a line using the local variable "a"
+  line(a, 0, a, height);
+}
+
